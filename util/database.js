0dd86434823dda2fe4e256ebd29c841ec72206d3
gaurav <gauravradadiya1412@gmail.com>
2022-07-15 12:05:42 +0530

    second commit

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'db_connect',
-    password: 'radadiya123'
+const sequelize = new Sequelize('node-complete','root','radadiya123',{
+    dialect:'mysql',
+    host :'localhost'
 });
 
-module.exports = pool.promise();
+
+module.exports = sequelize;
